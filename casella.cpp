d9ab662fdd433ce6b6fc807c67b4dd99a07c8a04
Davide <davide.bal97@gmail.com>
2018-03-20 10:47:15 +0100

    Creation of casella.h and casella.cpp

---
+++ casella.cpp
@@ -0,0 +1,7 @@
+/* ---------------------------------------------------------------
+ * Author: Davide Balestra - davide.balestra2@studio.unibo.it
+ * Date: 20-03-2018
+ * Description: Implementation of class casella and his derivates
+ * 		describes the behavior of the types of cells
+ * ---------------------------------------------------------------
+ */
---
+++ casella.h
@@ -0,0 +1,7 @@
+/* ------------------------------------------------------------------
+ * Author: Davide Balestra
+ * Date: 20-03-2018
+ * Descrption: header of class casella and his derivates
+ * 		decrvibes the types of cells
+ * ------------------------------------------------------------------
+ */
