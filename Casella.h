b0727e700fd26ea12f9dd255cde1ad9f1198e3ae
Davide <davide.bal97@gmail.com>
2018-03-20 11:42:28 +0100

    Modify: defined the subclasses; TODO: implementation of methods

--- Casella.h
+++ Casella.h
@@ -38,23 +38,43 @@ class Casella {
 };
 
 class Start:public Casella{
+	public:
+		Start(int t, string n, string d);
+		void action(Personaggio *p);
 };
 
 class Bridge:public Casella{
+	public:
+		Bridge(int t, string n, string d);
+		void action(Personaggio *p);
 };
 
 class Inn:public Casella{
+	public:
+		Inn(int t, string n, string d);
+		void action(Personaggio *p);
 };
 
 class Prision:public Casella{
+	public:
+		Prision(int t, string n, string d);
+		void action(Personaggio *p);
 };
 
 class Labirinth:public Casella{
+	public:
+		Labirinth(int t, string n, string d);
+		void action(Persionaggio *p);
 };
 
 class Skull:public Casella{
+	public:
+		Skull(int t, string n, string d);
+		void action(Personaggio *p);
 }
 
 class End:public Casella{
+	public: 
+		End(int t, string n, string d);
+		void action(Personaggio *p);
 };
-
