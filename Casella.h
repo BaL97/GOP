b4634af00491716b24685a3fbcfc4112f73432b8
Davide <davide.bal97@gmail.com>
2018-03-20 11:26:16 +0100

    Modify: defined the header of class Casella, todo: Define the subclasses

---
+++ Casella.h
@@ -0,0 +1,43 @@
+/* ------------------------------------------------------------------
+ * Author: Davide Balestra
+ * Date: 20-03-2018
+ * Descrption: header of class casella and his derivates
+ * 		decrvibes the types of cells
+ * ------------------------------------------------------------------
+ */
+
+#ifndef CELL_H
+#define CELL_H
+
+class Casella {
+
+	protected:
+		int type; //type of cell
+		string name, description; //name and description of the type
+		Personaggio *p; //pointer to character overflow list
+		Casella *next; //next cell in the map
+		Casella *prec; //previous cell in the map
+
+	public:
+		//Default Constructor
+		Casella();
+
+		//set methods
+		void set_type(int t);
+		void set_name(string n);
+		void set_desc(string d);
+		void set_p(personaggio *p);
+		void set_next(casella *n);
+		void set_prec(casella *p);
+
+		//get methods
+		int get_type();
+		string get_name();
+		string get_desc();
+		Personaggio *get_p();
+		Casella *get_next();
+		Casella *get_prec();
+
+		void display();
+
+}
--- casella.h
+++
@@ -1,7 +0,0 @@
-/* ------------------------------------------------------------------
- * Author: Davide Balestra
- * Date: 20-03-2018
- * Descrption: header of class casella and his derivates
- * 		decrvibes the types of cells
- * ------------------------------------------------------------------
- */
