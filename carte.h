ced4c9a04606b70863612997f0db452171344ac7
Arpa97 <andrea.darpa@studio.unibo.it>
2018-04-24 15:37:33 +0200

    Creato il mazzo. la classe carte deve subire ancora delle modifiche sui costruttori come deciso tramite le funzioni get e set.

--- carte.h
+++ carte.h
@@ -2,31 +2,39 @@
 #include <cstdlib>
 #include <string.h>
 #include <cstring>
+#include "cell.h"
 
 class Carte{
-private:
+protected:
     string Name;
     string Description;
     int Id;
-    
+    int Num;
+public:
+    void message(string Name, string Description);
+    void getName();
+    void getDescription();
+    void getId();
+    void setName(string Name);
+    void setDescription(string Description);
+    void setId(int Id);
+    void evento(personaggio p);
+    void eventoDado(personaggio p, dado d);
 };
 class Carta_vuota:public Carte{
 public:
-    Carta_vuota(string N ="carta vuota", string D="non fa nulla", int I=1);
-    void message();
+    Carta_vuota(string N ="carta vuota", string D="non fa nulla", int I=1);//10
+ 
 };
 class Carta_Avanti:public Carte{
 public:
     Carta_Avanti(string N="AVANTI TUTTA!", string D="avanti di 1", int I=2);
-    void message();            //messaggio che stampa la descrizione della carta su terminale
-    void evento (personaggio P)                //funzione ipotetica per spostare in avanti il personaggio. La gestione sarà determinata dalla classe personaggio.
+     evento ()                //funzione ipotetica per spostare in avanti il personaggio. La gestione sarà determinata dalla classe personaggio. 10
 };
 class Carta_Turno:public Carte{
 public:
     Carta_Turno(string N="BLOCCATO", string D="ops, sei bloccato per un turno...", int I=3);
-    void message();
-    
-    void evento(personaggio P);//ipotizzando che la gestione del turno venga esguita tramite un Booleano TRUE quando può muoversi e FALSE quando no
+     evento();//ipotizzando che la gestione del turno venga esguita tramite un Booleano TRUE quando può muoversi e FALSE quando no. 5
     
 };
 
@@ -35,25 +43,25 @@ public:
 class Carta_Tira_Avanti:public Carte{
 public:
     Carta_Tira_Avanti(string N="TIRA ANCORA!", string D="e vai avanti", int I=4);
-    void message();            //messaggio che stampa la descrizione della carta su terminale
     
-    void evento(personaggio P, dado D);
+  eventoDado();
     
     
-};
+}; //5
 
 class Carta_Tira_Indietro:public Carte{
 public:
     Carta_Tira_Indietro(string N="TIRA ANCORA!", string D="e vai indietro", int I=5);
-    void message();            //messaggio che stampa la descrizione della carta su terminale
     
-    void evento(personaggio P, dado D);
+     eventoDado();
     
-};
+};//5
 
 class Carta_Start:public Carte{
 public:
     Carta_Start(string N="TANTO VA LA GATTA AL LARDO...", string D="(che) Torna al punto di partenza", int I=5);
-    void message();
-};
+     evento();
+};//5
+
+
 
---
+++ mazzo.hpp
@@ -0,0 +1,13 @@
+//
+//  Mazzo.hpp
+//  
+//
+//  Created by Andrea D'Arpa on 24/04/18.
+//
+
+#ifndef Mazzo_hpp
+#define Mazzo_hpp
+
+#include <stdio.h>
+
+#endif /* Mazzo_hpp */
