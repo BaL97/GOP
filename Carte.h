f0d946640cc30a2c6f2d69f631495b0b37bc85ff
Davide <davide.balestra2@studio.unibo.it>
2018-05-27 16:41:33 +0200

    Aggiunta delle classi Mazzo e Carte

---
+++ Carte.h
@@ -0,0 +1,60 @@
+//
+// Created by Andrea D'Arpa on 02/05/18.
+//
+
+#ifndef GOP_CARTE_H
+#define GOP_CARTE_H
+
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstring>
+#include "player.h"
+using namespace std;
+
+class Carte{
+protected:
+    string Name;
+    string Description;
+    int Id;
+public:
+    string getName();
+    string getDescription();
+    int getId();
+    void setName(string Name);
+    void setDescription(string Description);
+    void setId(int id);
+    void messaggio();
+};
+
+class Carta_vuota:public Carte{
+public:
+    Carta_vuota();
+};
+
+class Carta_Avanti:public Carte{
+public:
+    Carta_Avanti();
+};
+
+class Carta_Turno:public Carte{
+public:
+    Carta_Turno();
+};
+
+class Carta_Tira_Avanti:public Carte{
+public:
+    Carta_Tira_Avanti();
+};
+
+class Carta_Tira_Indietro:public Carte{
+public:
+    Carta_Tira_Indietro();
+  };
+
+class Carta_Start:public Carte{
+public:
+    Carta_Start();
+};
+#endif //GOP_CARTE_H
---
+++ Mazzo.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Andrea D'Arpa on 02/05/18.
+//
+
+#include "Mazzo.h"
+ Mazzo::Mazzo() {             //genera il mazzo di carte ordinato.
+        for (int i=0; i<10; i++){
+            CartaMazzoEasy[i] = new Carta_vuota;
+        }
+        for (int i=10; i<20; i++){
+            CartaMazzoEasy[i] = new Carta_Avanti;
+        }
+        for (int i=20; i<25; i++){
+            CartaMazzoEasy[i]= new Carta_Turno;
+        }
+    for (int i=25; i<30; i++) {
+        CartaMazzoEasy[i] = new Carta_Tira_Avanti;
+    }
+
+    for (int i=30; i<35; i++){
+        CartaMazzoEasy[i]= new Carta_Tira_Indietro;
+    }
+    for (int i=35; i<40; i++){
+        CartaMazzoEasy[i]= new Carta_Start;
+    }
+
+}
+
+void Mazzo::Mischia(){
+    int i;
+    i = 40;
+    Carte *tmp;
+    srand(time(nullptr));
+    for (int p=0; p<40; p++){       //algoritmo che "mischia" le carte, generando un intero random che indicherà la posizione
+        int k;                      //da scambiare con l'ultimo elemento della lista, che andrà poi a scalare perchè già "scambiato"
+        k = rand() % i;
+            tmp = CartaMazzoEasy[k];
+            CartaMazzoEasy[k] = CartaMazzoEasy[i-1];
+            CartaMazzoEasy[i] = tmp;
+        i--;                        //decremento dell' indice dell'ultima posizione
+    }
+}
+
+
+Carte Mazzo::Pesca(){
+    segnalino ++;                          //contatore che tiene conto della carta pescata e della prossima carta da pescare
+    return *CartaMazzoEasy[segnalino - 1];
+}
+
+void Mazzo::stampa(){
+    int k=1;                            //funzione temporanea per il test!!!!DA ELIMINARE!!!
+    for (auto &i : CartaMazzoEasy) { cout << i->getName() << " - "<< k <<" - " << i->getDescription() << endl ; k++;}
+}
