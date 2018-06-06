f5bef7ba80cd20b76c4354ad1d455eea95a133a9
Davide <davide.balestra2@studio.unibo.it>
2018-06-06 11:15:09 +0200

    generation of map

--- map.cpp
+++ map.cpp
@@ -8,4 +8,16 @@
 
 	Map::Map(bool mode){
 		this->init=new Start();
+		generateMap(mode); //generate the map(Random)
+	}
+
+	void Map::generateMap(bool mode){
+		int n=this->calcNBox(mode);		//calc the number of boxes
+	}
+
+	int Map::calcNBox(bool mode){
+		if(!mode){	//easy mode -> map length between 49-63
+
+		}
+		else		//hard mode -> map length between 64-90
 	}
--- map.h
+++ map.h
@@ -10,4 +10,6 @@ class Map{
 public:
 	Start *init;		//Start Box (first of the map)
 	Map(bool mode);		//Create the map (Hard/Easy MODE)
+	void generateMap(bool mode);
+	int calcNBox(bool mode);
 };
